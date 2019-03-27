5773db2fb06858e16e4bb7881ceed68874d5d7e5
Stanislav <stanislavhacaturov.exceedteam@gmail.com>
2019-03-27 17:21:03 +0300

    27 March

--- script.js
+++ script.js
@@ -2,71 +2,146 @@ const ulElement = document.getElementById('list');
 
 let todoList = [];
 
-function add(){
-    if ($('#input').val() == ''){
-        alert("Введите текст!")
-    } else{
+function add() {
+    if ($('#input').val() == '') {
+        alert("Введите текст!");
+    } else {
         todoList.unshift({
-        content: $('#input').val(),
-        done: false,
-        selected: false
-    });
-    $('#input').val('');  
-    upgradeView ();
-    return todoList;}
+            content: $('#input').val(),
+            done: false,
+            selected: false
+        });
+        $('#input').val(''); 
+        upgradeView();
+        return todoList;
+    }
 }
-$('#add').click(function (){
+$('#add').click(function () {
     add();
 });
-$('#input').keydown(function(event){
-    if(event.which === 13)
-       {
-          add();
-       }
+$('#input').keydown(function (event) {
+    if (event.which === 13) {
+        add();
+    }
 });
 
-function upgradeView (){
+function upgradeView() {
 
-     ulElement.innerHTML = '';
+    AllActiveCompleted();
 
-    for (let index = 0; index < todoList.length; index++){
+    ulElement.innerHTML = '';
 
-        const todoItem = todoList[index]
+    for (let index = 0; index < todoList.length; index++) {
 
-        const liElement = $('<li>', {className: 'shape', id: 'liElement'}).appendTo(ulElement);
+        const todoItem = todoList[index];
 
-        const divElement = $('<div>', {className: 'list-group form-check', id: 'divElement'}).appendTo(liElement);
+        const names = ['red','green','yellow'];
+        const color = names[Math.floor(Math.random()*names.length)];
+
+        const liElement = $('<li>', {
+            class: 'list-group-item',
+            id: 'liElement'
+        });
+        
+        liElement.addClass(color);
+
+        liElement.appendTo(ulElement);
+
+        const divElement = $('<div>', {
+            class: 'list-group form-check',
+            id: 'divElement'
+        }).appendTo(liElement);
 
         const lebelElement = $('<lebel>', {
-            className: 'form-check-lebel', 
-            id: 'liElement',
-            html: todoItem.content})
-        if (todoItem.done) {className += ' todoDone'}
+            class: 'form-check-lebel',
+            id: 'lebelElement',
+            text: todoItem.content
+        });
+
+        if (todoItem.done) {
+            lebelElement.addClass(' todoDone')
+        }
+
         lebelElement.appendTo(divElement);
 
         const buttonDoneElement = $('<button>', {
-            className: 'btn btn_outline-primary', 
+            class: 'btn btn_outline-primary',
             id: 'buttonDoneElement',
             type: 'button',
             html: 'Done'
         }).appendTo(divElement);
 
         const buttonRemoveElement = $('<button>', {
-            className: 'btn btn_outline-danger', 
+            class: 'btn btn_outline-primary',
             id: 'buttonRemoveElement',
             type: 'button',
             html: 'Delete'
         }).appendTo(divElement);
 
-        $(buttonDoneElement).click(function done(){
+        $(buttonDoneElement).click(function done() {
             todoItem.done = !todoItem.done;
-            upgradeView ();
-        })
+            upgradeView();
+        });
 
-        $(buttonRemoveElement).click(function remove(){
-            todoList = todoList.filter( currentTodoItem => currentTodoItem !== todoItem);
-            upgradeView ();
-        })
-    }  
+        $(buttonRemoveElement).click(function remove() {
+            todoList = todoList.filter(currentTodoItem => currentTodoItem !== todoItem);
+            upgradeView();
+        });
+
+        lebelElement.dblclick(function edit() {
+            $('#input').val(todoItem.content);
+            todoList = todoList.filter(currentTodoItem => currentTodoItem !== todoItem)
+        });
+    }
 }
 
+function AllActiveCompleted() {
+
+    const activeTodo = todoList.filter(item => item.done === false).length;
+    inProcess.innerHTML = ": " + (activeTodo);
+
+    const okey = todoList.slice(todoList.done);
+    all.innerHTML = ": " + (okey.length);
+
+    complete.innerHTML = ": " + (okey.length - activeTodo);
+}
+
+$('#Completed').click(function Completed() {
+    const knopka1 = todoList.filter(item => item.done === true);
+    let temp = todoList;
+    todoList = knopka1;
+    upgradeView();
+    todoList = temp;
+});
+
+$('#Active').click(function Active() {
+    const knopka2 = todoList.filter(item => item.done === false);
+    let temp = todoList;
+    todoList = knopka2;
+    upgradeView();
+    todoList = temp;
+});
+
+$('#All').click(function All() {
+    upgradeView();
+});
+
+$('#removeAction').click(function removeAction() {
+    todoList = todoList.filter(todoItem => !todoItem.done);
+    upgradeView();
+});
+
+$('#doneAction').click(function doneAction() {
+    for (const todoItem of todoList) {
+        todoItem.done = !todoItem.done;
+    }
+    upgradeView();
+});
+
+$('#search').click(function search() {
+    const sear = todoList.filter(todoItem => todoItem.content == $('#input').val());
+    let temp = todoList;
+    todoList = sear;
+    upgradeView();
+    todoList = temp;
+});
